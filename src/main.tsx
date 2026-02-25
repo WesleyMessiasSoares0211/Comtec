f66c48572b6379847af0928a2f9c1d0fb3201d1e
WesleyMessiasSoares0211 <62735218+WesleyMessiasSoares0211@users.noreply.github.com>
2026-02-25 16:00:08 -0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; // Asegúrate de que tus estilos globales estén aquí
+import './index.css'; 
 import { AuthProvider } from './hooks/useAuth'; 
-// IMPORTANTE: Si tu AuthProvider no está en ./hooks/useAuth, 
-// ajusta esta ruta a donde tengas tu contexto (ej: ./context/AuthContext)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
