dd6b74315a10c12a1d3a905e3b525f4115857dfa
WesleyMessiasSoares0211 <62735218+WesleyMessiasSoares0211@users.noreply.github.com>
2026-02-25 16:05:15 -0300

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css'; 
-import { AuthProvider } from './hooks/useAuth'; 
+import './index.css';
+import { AuthProvider } from './hooks/useAuth'; // Esto ahora funcionará gracias al cambio a .tsx
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
